080e88718fea262ca818cbd8720b157c2e9592d2
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-09-17 22:00:46 -0300

    detalles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './app.js'
 import "./index.css"
-import { StrictMode } from 'react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
   <App ></App>
-  </StrictMode>
 )
