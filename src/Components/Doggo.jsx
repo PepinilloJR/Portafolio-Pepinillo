21a468a54df2f08319e185f5489bc34eedfe19ac
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-09-21 02:20:09 -0300

    programacion asincrona be like

--- src/Components/Doggo.jsx
+++ src/Components/Doggo.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import imgSrc from "../archivos/Latest.webp";
 import imgSrc1 from "../archivos/a1.png";
 import imgSrc2 from "../archivos/a2.png";
@@ -7,6 +7,11 @@ import imgSrc3 from "../archivos/a3.png";
 import imgSrc4 from "../archivos/a4.png";
 import imgSrc5 from "../archivos/a5.png";
 import imgSrc6 from "../archivos/a6.png";
+import imgSrc7 from "../archivos/t1.png";
+import imgSrc8 from "../archivos/t2.png";
+import imgSrc9 from "../archivos/t3.png";
+import imgSrc10 from "../archivos/t4.png";
+import imgSrc11 from "../archivos/t5.png";
 
 export function Doggo() {
 
@@ -15,37 +20,93 @@ export function Doggo() {
     const [dogTouched, setDogTouched] = useState(false)
 
     const [wagged, setWagged] = useState(false)
-    
+    const [animation, setAnimation] = useState(0)
+
+    // se requiere transition y speaked para la otra animacion por el hecho de que animation y wagged puede cambiar asincronamente en un useEffect 
+    // y mandarle otro valor que no conviene
+    const [transition, setTransition] = useState(0)
     const [speaked, setSpeaked] = useState(false)
 
-    const [animation, setAnimation] = useState(0)
 
     const [sprites1, setSprites1] = useState([imgSrc2, imgSrc1, imgSrc3])
 
     const [sprites2, setSprites2] = useState([imgSrc5, imgSrc4, imgSrc6])
 
-    setTimeout(()=> {
-        if (animation === 0) {
-            setWagged(false)
-            setAnimation(1);
-        } else if (animation === 1 && !wagged) {
-            setAnimation(2)
-        } else if (animation === 2 && !wagged) {
-            setAnimation(1)
-            setWagged(true)
-        } else if (wagged) {
-            setAnimation(0)
-        }
-    }, 170)
+    const [sprites3, setSprites3] = useState([imgSrc7, imgSrc8, imgSrc9, imgSrc10, imgSrc11])
 
-    if (dogSelected) {
+    const UseEffectejecutandose = useRef(false)
+    const [estadoUseEffect, setEstadoUseEffect] = useState(false)
+
+    // ESTA SOLUCION ES PORQUE SOY UN BURRO CON LAS PROMISES
+    // por cierto, el useEffect NO puede devolver una promesa asi que la funcion asyncrona debemos definirla dentro del useEffect
+
+    // esta parte puede mejorar, y lo hara en el futuro!!
+    useEffect(() => {
+
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        const animar = async () => {
+            // uso esta referencia para que no se acumulen diferentes useEffect
+            UseEffectejecutandose.current = true;
+            var cambio = transition
+            await sleep(200);
+            if (!dogTouched) {
+                if (animation === 0) {
+                    setWagged(false);
+                    setAnimation(1)
+                } else if (animation === 1 && !wagged) {
+                    setAnimation(2)
+                } else if (animation === 2 && !wagged) {
+
+                    setAnimation(1)
+                    setWagged(true);
+                } else if (wagged) {
+                    setAnimation(0)
+                }
+            } else {
+
+                // por ejemplo, aca podria pasa si usara wagged, que si, al clickear inicializa en false, pero podria pasar que justo se estaba ejecutando un useEffect anterior
+                // y cuyo resultado fuera justo coincidentemente que speaked es true, esto saltearia el primer if y pasaria directamente al segundo, cancelando la animacion
+                if (transition < 4 && speaked === false) {
+                    cambio = transition + 1
+                } else if (transition === 0) {
+                    setDogTouched(false);
+                } else {
+                    cambio = transition - 1
+                }
+
+                if (transition === 4) {
+                    setSpeaked(true);
+                }
+                setTransition(cambio)
+            }
+
+
+            UseEffectejecutandose.current = false;
+            setEstadoUseEffect(!estadoUseEffect)
+        };
+
+        if (UseEffectejecutandose.current === false) {
+            animar()
+        }
+    }, [estadoUseEffect])
+
+
+
+    if (dogSelected && !dogTouched) {
         return (
         <div className="Sector3">
-            <img onMouseLeave={() => {setDogSelected(false);}} className="windowsPerro" src={sprites2[animation]} alt="" />
+            <img onClick={() => {setDogTouched(true); setTransition(0); setSpeaked(false)}} onMouseLeave={() => {setDogSelected(false)}} className="windowsPerro" src={sprites2[animation]} alt="" />
         </div>
         )
     } else if (dogTouched) {
-
+        return (
+            <div className="Sector3">
+                <img className="windowsPerro" src={sprites3[transition]} alt="" />
+            </div>
+            )
 
     } else {
     return (
