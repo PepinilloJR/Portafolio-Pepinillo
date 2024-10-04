bfc1ec783f13120a70c34e0e739c67269fce67bb
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-10-04 14:39:25 -0300

    about me (base)

---
+++ src/Components/pages/aboutme/ContenidoAboutme.jsx
@@ -0,0 +1,38 @@
+
+
+
+
+export function ContenidoAboutMe() {
+    return (
+        <>
+            <div className="Presentacion" >
+                <div className="Titulo">
+                    About me!!!
+                </div>
+                <div className="Subtitulo">
+                    This page is dedicated to highlighting my professional traits and skills, rather than focusing on personal aspects.
+                </div>
+            </div>
+            <div className="ContenedorParrafos">
+                <div className="Parrafo">
+                    I have always had an interest in informatics, which naturally led me to coding. As a result,
+                    I’ve gained experience in various programming languages, libraries, and frameworks, including Python, JS, C#, and C++.
+                    (However, I would say that JS and C# are my main ones)
+                </div>
+                <div className="Parrafo">
+                    On the other hand, I am well-versed in libraries like React and frameworks such as Express.js (perhaps even a little too much, as switching back to plain JS
+                    has become a mental exercise :b)
+                </div>
+                <div className="Parrafo">
+                    i am also experienced in version control using Git and hosting projects on platforms like GitHub Pages, where this porfolio is currently hosted.
+                </div>
+                <div className="Parrafo">
+                    Still, I have much to learn, and I make the most of every opportunity to do so.
+
+                    Currently, I am in my third year of studying Systems Engineering at the UTN-FRC Universidad Tecnológica Nacional - Facultad Regional Córdoba in Argentina
+                </div>
+            </div>
+        </>
+
+    )
+}
