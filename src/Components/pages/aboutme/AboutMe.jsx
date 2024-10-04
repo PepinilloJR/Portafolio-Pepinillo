78ee621e10bdca2fb4516dc9c306c8723feb1a88
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-10-04 14:39:25 -0300

    about me (base)

---
+++ src/Components/pages/aboutme/AboutMe.jsx
@@ -0,0 +1,23 @@
+
+import "../../css/general.css"
+import { Datos } from "../../Datos.jsx"
+import { ContenidoAboutMe } from "./ContenidoAboutme.jsx"
+import { Menu } from "../../Menu.jsx"
+
+
+import "../../css/AboutMe.css"
+
+import { Rover } from "../../Doggo.jsx"
+
+export function AboutMe() {
+    return (
+        <div className="Container">
+            <Datos></Datos>
+            <div className="Sector2">
+                <Menu></Menu>
+                <ContenidoAboutMe></ContenidoAboutMe>
+            </div>
+            <Rover></Rover>
+        </div>
+    )
+}
--- src/Components/routes.js
+++ src/Components/routes.js
@@ -1,12 +1,14 @@
 import {HashRouter, Route, Routes} from 'react-router-dom';
 import { Projects } from './pages/Projects/projects.jsx';
 import {Home} from './pages/Home/Home.jsx';
+import { AboutMe } from './pages/aboutme/AboutMe.jsx';
 export function Router() {
 
     return (
         <Routes >
             <Route path='/' element={<Home></Home>}></Route>
             <Route path='/Proyectos' element={<Projects></Projects>}></Route>
+            <Route path='/Aboutme' element={<AboutMe></AboutMe>}></Route>
             <Route path="/projects.json" exact={true} render={() => null} />
         </Routes>
     )
