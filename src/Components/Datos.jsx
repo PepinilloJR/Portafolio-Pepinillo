9db46c18d81b0703345aa3ad06eefbc70d1c0ec9
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-11-20 08:58:01 -0300

    cambios en el css de datos de perfil

--- src/Components/Datos.jsx
+++ src/Components/Datos.jsx
@@ -1,5 +1,5 @@
 import "./css/general.css"
-import { FaInstagram, FaGithub, FaSteam, FaTelegram, FaLastfm, FaHeart    } from "react-icons/fa";
+import { FaInstagram, FaGithub, FaSteam, FaTelegram, FaLastfm, FaHeart, FaLinkedin    } from "react-icons/fa";
 import imgSrc from '../archivos/drgonzo.png';
 import { useState } from "react";
 
@@ -26,6 +26,10 @@ export function Datos() {
                 <FaGithub></FaGithub>
                     GitHub
                 </a>
+                <a className="Red" target="blank" href="https://www.linkedin.com/in/francisco-monasterolo-16926822a/">
+                <FaLinkedin></FaLinkedin>
+                    Linkedin
+                </a>
                 <a className="Red" target="blank" href="https://t.me/PepinilloJR">
                 <FaTelegram ></FaTelegram >
                 Telegram</a>
@@ -46,6 +50,10 @@ export function Datos() {
                 <a className="Red" target="blank" href="https://github.com/PepinilloJR">
                 <FaGithub></FaGithub>
                     GitHub
+                </a>
+                <a className="Red" target="blank" href="https://www.linkedin.com/in/francisco-monasterolo-16926822a/">
+                <FaLinkedin></FaLinkedin>
+                    Linkedin
                 </a>
                 <a className="Red" target="blank" href="https://t.me/PepinilloJR">
                 <FaTelegram ></FaTelegram >
